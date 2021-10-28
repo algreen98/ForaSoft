c7d86110a678d8fd4af80ff4b2b0f7b7855f5e75
alexgreen98 <al_green98@mail.ru>
2021-10-28 07:08:35 +0300

    start

---
+++ ForaSoftBrovkin/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  ForaSoftBrovkin
+//
+//  Created by Brovkin Alexander on 28.10.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ ForaSoftBrovkin/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ForaSoftBrovkin
+//
+//  Created by Brovkin Alexander on 28.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
